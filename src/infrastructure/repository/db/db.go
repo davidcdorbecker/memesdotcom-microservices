ee1e35373deb4e2cb58708f8fd1f8c860df4e72b
David Castillo <david.castillo@venafi.com>
2020-12-29 21:25:23 -0600

    corrected typos

--- src/infrastructure/repository/db/db.go
+++ src/infrastructure/repository/db/db.go
@@ -27,20 +27,20 @@ func (db *dbRepository) CreateClient(user domain.User) _errors.RestError {
 	stmt, err := db.mysqlClient.Prepare(queryRegisterUser)
 	if err != nil {
 		log.Error("error when trying to prepare save user statement", err)
-		return _errors.NewInternalServerError("error when tying to save user")
+		return _errors.NewInternalServerError("error when trying to save user")
 	}
 	defer stmt.Close()
 
 	result, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated, user.Status, user.Password)
 	if err != nil {
 		log.Error("error when trying to save user", err)
-		return _errors.NewInternalServerError("error when tying to save user")
+		return _errors.NewInternalServerError("error when trying to save user")
 	}
 
 	userId, err := result.LastInsertId()
 	if err != nil {
 		log.Error("error when trying to get last insert id after creating a new user", err)
-		return _errors.NewInternalServerError("error when tying to save user")
+		return _errors.NewInternalServerError("error when trying to save user")
 	}
 	user.ID = userId
 
