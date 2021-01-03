8acbc9eb581ebd901d2ad9899066ff334f94ee2c
David Castillo <david.castillo@venafi.com>
2021-01-03 15:43:18 -0600

    init schema

---
+++ src/db/migration/init_schema.up.sql
@@ -0,0 +1,13 @@
+create table users
+(
+    id           int auto_increment
+        primary key,
+    first_name   varchar(45)                        null,
+    last_name    varchar(45)                        null,
+    email        varchar(45)                        not null,
+    date_created datetime default CURRENT_TIMESTAMP null,
+    status       varchar(45)                        null,
+    password     varchar(45)                        not null,
+    constraint email_UNIQUE
+        unique (email)
+);
